52fb5517b462a2de2e5d069f42364dd7dd93b204
Andrew54757 <andrews54757@gmail.com>
2016-09-10 20:32:53 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 const http = require('http')
 const https = require('https');
 module.exports = function(url,callback) {
-  var protocal,
+  var secure = false,
       host,
       path,
-      urlb;
-  
+      urlb,
+      port = "";,
+      a,
+      b,
+      c;
+a = url.split("://");
+if (a[0] == "https") secure = true; else secure = false;
+  b = a[1];
+  a = b.split("/");
+  host = a[0];
+  b = a.slice(1); // needed
+  a = b[b.length - 1];
+  a = a.split(":");
+  if (a[1] && !a[2]) {
+    port = parseInt(a[1])
+   b[b.length - 1] = a[0]; 
+  }
   
 }
