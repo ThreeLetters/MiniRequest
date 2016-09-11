6ea2937c44c9abec6a3f1efb7b8000feaea54f3d
Andrew54757 <andrews54757@gmail.com>
2016-09-10 21:18:00 -0400

    Update minified.js

--- minified.js
+++ minified.js
@@ -14,5 +14,5 @@ Copyright 2016 Andrew S
    limitations under the License.
 */
 
-module.exports = function(t,n){var e,l,i,o,s,u=require("http"),h=require("https"),r=!1,p="",c="";o=t.split("://"),r="https"==o[0]?!0:!1,s=o[1]?o[1]:o[0],o=s.split("/"),e=o[0],s=o[o.length-1].split(":"),s.length>1&&(c=parseInt(s[s.length-1]),o[o.length-1]=s.slice(0,s.length-1).join(":")),i=o[1]?o.slice(1).join("/"):"/",l=r?h:u;try{var a=l.request({host:e,path:i,port:c},function(t){t.setEncoding("utf8"),t.on("data",function(t){p+=t}),t.on("end",function(){n(!1,t.statusCode,p)})});a.end()}catch(d){n(d,null,null)}};
+module.exports = function(t,n){var e,l,i,o,s,h=require("http"),r=require("https"),u=!1,p="",c="";o=t.split("://"),u="https"==o[0]?!0:!1,s=o[1]?o[1]:o[0],o=s.split("/"),e=o[0],s=o[o.length-1].split(":"),s.length>1&&(c=parseInt(s[s.length-1]),o[o.length-1]=s.slice(0,s.length-1).join(":")),i=o[1]?o.slice(1).join("/"):"/",l=u?r:h;try{var a=l.request({host:e,path:i,port:c},function(t){t.setEncoding("utf8"),t.on("data",function(t){p+=t}),t.on("end",function(){n(!1,t,p)})});a.end()}catch(g){n(g,null,null)}};
 // replace "module.exports" with whatever variable you like if you would rather insert it right into your code
