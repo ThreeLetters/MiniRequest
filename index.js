a3c29ea14be184dbe1e1f4b90c5f13fdd2cd094d
Andrew54757 <andrews54757@gmail.com>
2016-09-10 20:43:52 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ const https = require('https');
 module.exports = function(url,callback) {
   var secure = false,
       host,
+      method,
       path,
-      urlb,
+      content = "",
       port = "";,
       a,
-      b,
-      c;
+      b;
 a = url.split("://");
 if (a[0] == "https") secure = true; else secure = false;
   b = a[1];
@@ -17,9 +17,30 @@ if (a[0] == "https") secure = true; else secure = false;
   b = a.slice(1); // needed
   a = b[b.length - 1];
   a = a.split(":");
-  if (a[1] && !a[2]) {
-    port = parseInt(a[1])
-   b[b.length - 1] = a[0]; 
+  if (a.length > 1) {
+    port = parseInt(a[a.length - 1])
+   b[b.length - 1] = a.slice(0,a.length - 1).join(":"); 
   }
-  
+  path = b.join("/");
+  if (secure) {
+    method = https;
+  } else {
+    method = http;
+  }
+ try {
+var request = method.request(options, function(res) {
+    res.setEncoding("utf8");
+    res.on("data", function (chunk) {
+        content += chunk;
+    });
+
+    res.on("end", function () {
+        callback(false,res.statusCode,body)
+    });
+});
+
+request.end();
+ } catch (e) {
+   callback(e,null,null);
+ }
 }
