92cdbcd661b721ab129857d1e1a7947eda6d40af
Andrew S <andrews54757@gmail.com>
2017-03-29 09:45:05 -0400

    Update minified.js

--- minified.js
+++ minified.js
@@ -14,7 +14,7 @@ Copyright 2016 Andrew S
    limitations under the License.
 */
 
-var querystring=require("querystring"),http=require("http"),https=require("https");
-module.exports=function(e,c,f){var d=!1,h="/",k="",l="",a,b;c=querystring.stringify(c);a=e.split("://");d="https"==a[0]?!0:!1;b=a[1]?a[1]:a[0];a=b.split("/");e=a[0];b=a[a.length-1].split(":");1<b.length&&(l=parseInt(b[b.length-1]),a[a.length-1]=b.slice(0,b.length-1).join(":"));a[1]&&(h+=a.slice(1).join("/"));d=d?https:http;try{var g=d.request({host:e,path:h,port:l,method:"POST",headers:{"Content-Type":"application/x-www-form-urlencoded","Content-Length":Buffer.byteLength(c)}},function(a){a.setEncoding("utf8");
-a.on("data",function(a){k+=a});a.on("end",function(){f(!1,a,k)})});g.on("error",function(a){f(a,null,null)});g.write(c);g.end()}catch(m){f(m,null,null)}};
+var http=require("http"),https=require("https"),querystring=require("querystring");
+module.exports=function(f,b,c){var e=!1;if(c)var e=querystring.stringify(b),g=c;else g=b;var h=!1;b="/";var l="";c="";var a,d;a=f.split("://");h="https"==a[0]?!0:!1;d=a[1]?a[1]:a[0];a=d.split("/");d=a[0].split(":");f=d[0];d[1]&&(c=parseInt(d[1]));a[1]&&(b+=a.slice(1).join("/"));h=h?https:http;try{var k=h.request(e?{host:f,path:b,port:c,method:"POST",headers:{"Content-Type":"application/x-www-form-urlencoded","Content-Length":Buffer.byteLength(e)}}:{host:f,path:b,port:c},function(a){a.setEncoding("utf8");
+a.on("data",function(a){l+=a});a.on("end",function(){g(!1,a,l)})});k.on("error",function(a){g(a,null,null)});e&&k.write(e);k.end()}catch(m){g(m,null,null)}};
 // replace "module.exports" with whatever variable you like if you would rather insert it right into your code
