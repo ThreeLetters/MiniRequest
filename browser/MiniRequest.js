5ad0245639a0338a26b36ceb3f006654d7a521fd
Andrew S <andrews54757@gmail.com>
2017-06-14 17:25:39 -0400

    Create MiniRequest.js

--- browser/MiniRequest.js
+++ browser/MiniRequest.js
@@ -13,62 +13,44 @@ Copyright 2016 Andrew S
    See the License for the specific language governing permissions and
    limitations under the License.
 */
-const http = require('http');
-const https = require('https');
-const querystring = require('querystring');
-module.exports = function(/**/) {
-   var url = arguments[0]
-   var toSend = false;
-   if (arguments[2]) { // POST
-      toSend = querystring.stringify(arguments[1]) // stringify
-         var callback = arguments[2];
-   } else var callback = arguments[1];
-  var secure = false,
-      host,
-      method,
-      path = "/",
-      content = "",
-      port = "",
-      urlSplit,
-      hostSplit;
-urlSplit = url.split("://"); // split url string into - method + "://" + host ...
-if (urlSplit[0] == "https") secure = true; else secure = false; // check if using https
-  urlSplit = (urlSplit[1]) ? urlSplit.slice(1).join("://") : urlSplit[0]; // remove https/http if there is one
-  urlSplit = urlSplit.split("/"); // split url by "/".
- 
-  hostSplit = urlSplit[0].split(":") // split host into - hostname + ":" + port
-   host = hostSplit[0]; // host
-  if (hostSplit[1]) { // there is a port
-    port = parseInt(hostSplit[1])
-  }
-  
-  if (urlSplit[1]) path += urlSplit.slice(1).join("/"); // Get path, excluding hostname
-   
-  if (secure) { // check if secure and set right class
-    method = https;
-  } else {
-    method = http;
-  }
- try { // error handling
-    
-    // check if it is a POST request.
-var options = (toSend) ? {host:host,path:path,port:port,method:"POST",headers: {'Content-Type': 'application/x-www-form-urlencoded','Content-Length': Buffer.byteLength(toSend)}} : {host:host,path:path,port:port}
-var request = method.request(options, function(res) {
-    res.setEncoding("utf8");
-    res.on("data", function (chunk) {
-        content += chunk;
-    });
 
-    res.on("end", function () { // done
-        callback(false,res,content)
-    });
-});
-    request.on('error',function(e) {
-       callback(e,null,null); // error
-    })
-if (toSend) request.write(toSend)
-request.end();
- } catch (e) {
-   callback(e,null,null); // error
- }
+window.request = function(/**/) {
+ var url = arguments[0],
+     post = undefined,
+     callback;
+   
+   if (arguments[2]) { // post
+      post = arguments[1];
+      callback = arguments[2];
+   } else callback = arguments[1];
+   try {
+   var xhr = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject("Microsoft.XMLHTTP"); // IE support
+   xhr.open(post ? 'POST' : 'GET', url);
+   xhr.onreadystatechange = function() {
+      if (xhr.readyState == 4) {
+      if (xhr.status === 200) {
+         callback(undefined,xhr,xhr.responseText);
+      } else {
+         callback(true,xhr,false);
+      }
+      
+    var body = xhr.responseText;
+    var res = xhr
+    }
+   };
+    if (post) {
+        xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+       
+       var toPost = [];
+       for (var i in post) {
+        toPost.push(encodeURIComponent(i) + '=' + encodeURIComponent(post[i])) 
+       }
+       
+       post = toPost.join("&")
+    }
+      
+   xhr.send(post);
+   } catch (e) {
+    callback(e);  
+   }
 }
