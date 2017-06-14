e8a72ab3d3819e313a04b12bd96ef938719f5117
Andrew S <andrews54757@gmail.com>
2017-06-14 17:38:28 -0400

    Create minified.js

--- minified.js
+++ minified.js
@@ -15,6 +15,6 @@ Copyright 2016 Andrew S
 */
 
 var http=require("http"),https=require("https"),querystring=require("querystring");
-module.exports=function(b,c,d){var e=!1;if(d){e=querystring.stringify(c);var f=d}else f=c;var g=!1;c="/";var l="";d="";var a=b.split("://");g="https"==a[0]?!0:!1;a=a[1]?a.slice(1).join("://"):a[0];a=a.split("/");var h=a[0].split(":");b=h[0];h[1]&&(d=parseInt(h[1]));a[1]&&(c+=a.slice(1).join("/"));g=g?https:http;b=b+"?"+Date.now();try{var m=e?{host:b,path:c,port:d,method:"POST",headers:{"Content-Type":"application/x-www-form-urlencoded","Content-Length":Buffer.byteLength(e)}}:{host:b,path:c,port:d},
-k=g.request(m,function(a){a.setEncoding("utf8");a.on("data",function(a){l+=a});a.on("end",function(){f(!1,a,l)})});k.on("error",function(a){f(a,null,null)});e&&k.write(e);k.end()}catch(n){f(n,null,null)}};
+module.exports=function(e,b,c){var d=!1;if(c){d=querystring.stringify(b);var f=c}else f=b;var g=!1;b="/";var l="";c="";var a=e.split("://");g="https"==a[0]?!0:!1;a=a[1]?a.slice(1).join("://"):a[0];a=a.split("/");var h=a[0].split(":");e=h[0];h[1]&&(c=parseInt(h[1]));a[1]&&(b+=a.slice(1).join("/"));g=g?https:http;b+="?"+Date.now();try{var m=d?{host:e,path:b,port:c,method:"POST",headers:{"Content-Type":"application/x-www-form-urlencoded","Content-Length":Buffer.byteLength(d)}}:{host:e,path:b,port:c},
+k=g.request(m,function(a){a.setEncoding("utf8");a.on("data",function(a){l+=a});a.on("end",function(){f(!1,a,l)})});k.on("error",function(a){f(a,null,null)});d&&k.write(d);k.end()}catch(n){f(n,null,null)}};
 // replace "module.exports" with whatever variable you like if you would rather insert it right into your code
